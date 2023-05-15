798708f034531e9b97fe9bcac4449a74a10cd4eb
HanadiAljeaidi <129965658+HanadiAljeaidi@users.noreply.github.com>
2023-05-15 14:17:50 +0400

    Update Print_Variables.cpp

--- Basics/Print_Variables.cpp
+++ Basics/Print_Variables.cpp
@@ -1,1 +1,26 @@
 
+/*GOAL: Practice writing to the console and learn 
+**the variables types available in C++
+**Print the sizes of each variable to the console.
+**Print them in the following order:
+**int, short, long, char, float, double, bool
+**
+**Use the command sizeof(variable type) ie: sizeof(int)
+*/
+
+
+#include <iostream>
+
+int main() 
+{
+    using namespace std;
+    cout<<"The size of int: "<<sizeof(int)<<"\n";
+    cout<<"The size of short: "<<sizeof(short)<<"\n";
+    cout<<"The size of long: "<<sizeof(long)<<"\n";
+    cout<<"The size of char: "<<sizeof(char)<<"\n";
+    cout<<"The size of float: "<<sizeof(float)<<"\n";
+    cout<<"The size of double: "<<sizeof(double)<<"\n";
+    cout<<"The size of bool: "<<sizeof(bool)<<"\n";
+    
+    return 0;
+}
