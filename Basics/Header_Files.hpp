ed4441d2377135a56180fca4a186204a232ad5e1
HanadiAljeaidi <129965658+HanadiAljeaidi@users.noreply.github.com>
2023-05-15 14:30:28 +0400

    Update Header_Files.hpp

--- Basics/Header_Files.hpp
+++ Basics/Header_Files.hpp
@@ -1,18 +1,19 @@
-/*Goal: practice using header files.
-**. 
-**Put in the header file every thing that is related to 
+/*This is a header file. 
+**Put in a header file every thing that is related to 
 **"how to do a task".
 **Put the "what to do" in the main program.
 */
 
-// #include <iostream>
-#include <string>
-#include "main.hpp"
 
-// using namespace std;
+#include <iostream>
 
-int main()
-{
-    cout<<"Hello, I use header files!";
-    return 0;
-}
+using namespace std;
+// int main()
+// {
+//     std::cout<<"How to do a task: "<<"\n";
+//     return 0;
+// }
+
+
+
+
