c943b70e0581db95cb315ab0ccf71f277af0e0cf
HanadiAljeaidi <Hanadi.Aljeaidi77@gmail.com>
2023-05-16 19:23:36 +0400

    Trying to commit changes from VSCode

---
+++ Functions/Functions_Prog1.cpp
@@ -0,0 +1,20 @@
+/*Goal: write a function called printMessage()
+**that prints: Functions
+*/
+
+#include<iostream>
+
+void printMessage();
+
+int main()
+{
+    printMessage();
+    
+    return 0;
+}
+
+    
+void printMessage() {
+        std::cout<<"Functions";
+        std::cout<<"Trying to committing changes using VSCode";
+    }
