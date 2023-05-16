2aa32ec4dbad70ea7650d75804daf3ff951416ae
HanadiAljeaidi <Hanadi.Aljeaidi77@gmail.com>
2023-05-16 12:02:35 +0400

    debugging

--- Control Flow/While_Loops_Programming_Quiz.cpp
+++ Control Flow/While_Loops_Programming_Quiz.cpp
@@ -31,7 +31,7 @@ int main()
             
             std::cin>>guess;
             std::cout<<guess<<"\n";
-        } else
+        } else {
             std::cout<<"The guessed number is right!";
         }
     
