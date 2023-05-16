4c69345c5ad91c7dafa3c3558ce8bd00a103c4b3
HanadiAljeaidi <Hanadi.Aljeaidi77@gmail.com>
2023-05-16 19:41:32 +0400

    Create FunctionwParam.cpp file

---
+++ Functions/FunctionwParam.cpp
@@ -0,0 +1,27 @@
+/*GOAL: Practice writing functions
+
+/*Write a function called printProduct that prints the results of the multiplication 
+**of two integers. 
+*/
+
+#include<iostream>
+
+void printProduct(int m1, int m2, int product);
+
+
+int main()
+{
+    int m1 = 4;
+    int m2 = 5;
+    int product;
+
+    product = m1 * m2;
+    
+    printProduct(m1, m2, product);
+    return 0;
+}
+
+
+void printProduct(int m1, int m2, int product){
+    std::cout<<"The product of m1 and m2 = "<<product<<"\n";
+}
