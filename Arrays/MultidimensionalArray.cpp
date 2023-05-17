5ef3fb44f4fee23dac81a5d3db97a19ab11f4fc7
HanadiAljeaidi <129965658+HanadiAljeaidi@users.noreply.github.com>
2023-05-17 12:07:59 +0400

    Create MultidimensionalArray.cpp

---
+++ Arrays/MultidimensionalArray.cpp
@@ -0,0 +1,16 @@
+/*Goal: practice using multidimensional arrays.
+**Write a program that will accept values for a 4x4 array 
+**and a vector of size 4. 
+**Use the dot product to multiply the array by the vector. 
+**Print the resulting vector. 
+*/
+
+#include<iostream>
+
+int main()
+{
+    //TODO: multiply a 4x4 array with vector of size 4. 
+    //Print the resultant product vector
+    
+    return 0;
+}
