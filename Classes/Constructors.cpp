b936ce0bd0a76db503ad710217c266bc0f050281
HanadiAljeaidi <129965658+HanadiAljeaidi@users.noreply.github.com>
2023-05-17 17:03:13 +0400

    Create Constructors.cpp

---
+++ Classes/Constructors.cpp
@@ -0,0 +1,43 @@
+/*
+The declaration for a constructor is:
+
+ClassName::ClassName();
+
+The definition of a constructor is:
+
+ClassName::ClassName()
+{
+     dataMemberName1 = value;
+     dataMemberName2 = value;
+     ...
+}
+*/
+
+
+/*           Task1             */
+
+
+/*Goal: practice creating and using classes.
+**Create a class called Cat.
+**Create the following members:
+**private members: name, breed, age
+**public members: setName, setBreed,setAge
+**getName, getBreed, getAge, printInfo*/
+
+#include"main.hpp"
+
+int main(){
+   Cat cat1,cat2;
+   cat1.setName("Kimmy");
+   cat2.setName("Bobby");
+   cat1.setBreed("calico");
+   cat2.setBreed("main coon");
+   cat1.setAge(4);
+   cat2.setAge(1);
+   
+   cat1.printInfo();
+   cout<<"\n";
+   cat2.printInfo();
+   
+   return 0;
+}
