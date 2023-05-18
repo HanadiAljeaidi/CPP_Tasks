a9d4f870918c22e9dfee3c0b3d670ec5cf0fae45
HanadiAljeaidi <129965658+HanadiAljeaidi@users.noreply.github.com>
2023-05-18 13:47:11 +0400

    Create Overloading_Constructors.hpp

---
+++ Overloading/Overloading_Constructors.hpp
@@ -0,0 +1,61 @@
+/*Create a class that has four constructors:
+* one that accepts no input parameters
+* one that accepts name
+* one that accepts license number
+* one that accepts name and license number
+* The default values are:
+name = NA
+license = 0
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Dog
+{
+    private:
+        string name;
+        int license;
+    public:
+        Dog();
+        Dog(string nameIn);
+        Dog(int licIn);
+        Dog(string nameIn, int licIn);
+        string getName();
+        int getLicense();
+};
+
+Dog::Dog()
+{
+    name = "NA";
+    license = 0;
+}
+
+Dog::Dog(string nameIn)
+{
+    name = nameIn;
+    license = 0;
+}
+
+Dog::Dog(int licIn)
+{
+    name = "NA";
+    license = licIn;
+}
+
+Dog::Dog(string nameIn, int licIn)
+{
+    name = nameIn;
+    license = licIn;
+}
+
+string Dog::getName()
+{
+    return name;
+}
+
+int Dog::getLicense()
+{
+    return license;
+}    
