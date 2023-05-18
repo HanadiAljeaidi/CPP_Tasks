4b67fdf67f2d325b6e25ab7b8f20a3615e03dde7
HanadiAljeaidi <129965658+HanadiAljeaidi@users.noreply.github.com>
2023-05-18 15:47:20 +0400

    Create Function_Templates.cpp

---
+++ Templates/Function_Templates.cpp
@@ -0,0 +1,11 @@
+/*Goal: Practice writing generic functions*/
+
+#include "main.hpp"
+
+int main()
+{
+    int a = 90;
+    int b = 1; 
+    std::cout<<a <<" + "<<b<<" = "<< sumTwo(a,b);
+    return 0;
+}
